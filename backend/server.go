6da836a106bc1b2143c1398d042c507a3475743f
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2024-12-24 12:44:48 +0200

    extremely basic upload

--- backend/server.go
+++ backend/server.go
@@ -1,47 +1,105 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
+	"text/template"
 
-	"github.com/gin-gonic/gin"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, hx-current-url, hx-request, hx-target")
-		c.Header("Access-Control-Allow-Credentials", "true")
+type MinIoClient struct {
+	client *minio.Client
+}
 
-		if c.Request.Method == http.MethodOptions {
-			c.AbortWithStatus(http.StatusOK)
-			return
+func (m *MinIoClient) uploadFile(ctx context.Context, file multipart.File, name string, contentType string) error {
+	bucketName := "test"
+	location := "test"
+
+	err := m.client.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		exists, errBucketExists := m.client.BucketExists(ctx, bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own hello%s\n", bucketName)
+		} else {
+			return err
 		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
 
-		c.Next()
+	info, err := m.client.PutObject(ctx, "my-bucker", name, file, -1, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", name, info.Size)
+	return nil
+}
+
+type TemplateData struct {
+	BackendURL string
+}
+
+func templateHandler(w http.ResponseWriter, r *http.Request) {
+	templatePath := filepath.Join("static", "index.html")
+	tmpl, err := template.ParseFiles(templatePath)
+	d := TemplateData{BackendURL: os.Getenv("BACKEND_URL")}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, d)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func main() {
-	r := gin.Default()
-
-	r.Static("/static", "./static/")
-	r.LoadHTMLFiles("./static/index.html")
-
-	r.Use(CORSMiddleware())
-
-	r.GET("/", func(c *gin.Context) {
-		backendURL := os.Getenv("BACKEND_URL")
-		c.HTML(200, "index.html", gin.H{
-			"BackendURL": backendURL,
-		})
+	client, err := minio.New(os.Getenv("MINIO_URL"), &minio.Options{
+		Creds: credentials.NewStaticV4(os.Getenv("MINIO_ROOT_USER"), os.Getenv("MINIO_ROOT_PASSWORD"), ""),
 	})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	if err != nil {
+		log.Fatalf("Failed to start file server client, %s", err)
+	}
+
+	minIoClient := MinIoClient{client: client}
+	http.HandleFunc("/", templateHandler)
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		err := r.ParseMultipartForm(10 << 24)
+
+		if err != nil {
+			http.Error(w, "Failed to validate payload", http.StatusBadRequest)
+			return
+		}
+
+		file, handler, err := r.FormFile("file")
+
+		if err != nil {
+			http.Error(w, "Failed to validate payload", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		err = minIoClient.uploadFile(r.Context(), file, handler.Filename, handler.Header.Get("Content-Type"))
+		if err != nil {
+			fmt.Fprintf(w, "Somethings up: %s", err)
+		}
 	})
-	r.Run()
+
+	log.Println("Starting a server at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
